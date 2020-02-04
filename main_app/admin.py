abf1c287db2753240a2aae841d6b4c80a887a7f6
killerwhiteshark <killerwhiteshark@users.noreply.github.com>
2020-02-04 14:26:39 -0800

    Add user

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Shark
+from .models import Shark, Feeding
 # Register your models here.
 
 admin.site.register(Shark)
+admin.site.register(Feeding)
