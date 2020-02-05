983a3d3104c7e62943e5d80f42f87ea3460530a4
killerwhiteshark <killerwhiteshark@users.noreply.github.com>
2020-02-05 09:29:20 -0800

    Add feeding update function

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
     ('B', 'Breakfast'),
@@ -18,6 +19,9 @@ class Shark(models.Model):
   def get_absolute_url(self):
     return reverse('detail', kwargs={'shark_id': self.id})
 
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
 class Feeding(models.Model):
   date = models.DateField()
   meal = models.CharField(
@@ -31,3 +35,6 @@ class Feeding(models.Model):
   def __str__(self):
   # Nice method for obtaining the friendly value of a Field.choice
     return f"{self.get_meal_display()} on {self.date}"
+
+  class Meta:
+    ordering = ['-date']
