e3ee060c52fd270481124dad06ad956d15019e4f
killerwhiteshark <killerwhiteshark@users.noreply.github.com>
2020-02-06 09:24:40 -0800

    Add User sign in functionalilty

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,10 @@ from .models import Shark, Toy
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from .forms import FeedingForm
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse ### ADDED HTTPRESPONSE MODULE FOR ONLY RESPONSES
 # Create your views here.
 
@@ -26,16 +30,19 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def sharks_index(request):
-  sharks = Shark.objects.all()
+  sharks = Shark.objects.filter(user=request.user)
   return render(request, 'sharks/index.html', { 'sharks': sharks })
 
+@login_required
 def sharks_detail(request, shark_id):
   shark = Shark.objects.get(id=shark_id)
   toys_shark_doesnt_have = Toy.objects.exclude(id__in = shark.toys.all().values_list('id'))
   feeding_form = FeedingForm()
   return render(request, 'sharks/detail.html', { 'shark': shark,'feeding_form': feeding_form, 'toys': toys_shark_doesnt_have })
 
+@login_required
 def add_feeding(request, shark_id):
   # create the ModelForm using the data in request.POST
   form = FeedingForm(request.POST)
@@ -48,7 +55,7 @@ def add_feeding(request, shark_id):
     new_feeding.save()
   return redirect('detail', shark_id=shark_id)
 
-class SharkCreate(CreateView):
+class SharkCreate(LoginRequiredMixin, CreateView):
   model = Shark
   fields = ['name', 'species', 'description']
   def form_valid(self, form):
@@ -56,30 +63,32 @@ class SharkCreate(CreateView):
     return super().form_valid(form)
 
 
-class SharkUpdate(UpdateView):
+class SharkUpdate(LoginRequiredMixin, UpdateView):
   model = Shark
   # Let's disallow the renaming of a Shark by excluding the name field!
   fields = ['species', 'description']
 
-class SharkDelete(DeleteView):
+class SharkDelete(LoginRequiredMixin, DeleteView):
   model = Shark
   success_url = '/sharks/'
 
+@login_required
 def assoc_toy(request, shark_id, toy_id):
   Shark.objects.get(id=shark_id).toys.add(toy_id)
   return redirect('detail', shark_id=shark_id)
 
+@login_required
 def unassoc_toy(request, shark_id, toy_id):
   Shark.objects.get(id=shark_id).toys.remove(toy_id)
   return redirect('detail', shark_id=shark_id)
 
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
   model = Toy
 
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
   model = Toy
 
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
   model = Toy
   fields = '__all__'
 
@@ -87,7 +96,26 @@ class ToyUpdate(UpdateView):
   model = Toy
   fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
   model = Toy
   success_url = '/toys/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
 ##########################################################
